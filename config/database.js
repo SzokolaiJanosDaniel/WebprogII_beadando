06b6db2c197a6e84bd8c42a42734cfe424a8130a
Szokolai János Dániel <szokkancs@icloud.com>
2025-11-27 21:41:45 +0100

    Delete config directory

--- config/database.js
+++
@@ -1,5 +0,0 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-dotenv.config();
-const url = process.env.DATABASE_URL || 'sqlite:db.sqlite';
-export const sequelize = new Sequelize(url, { logging: false });
